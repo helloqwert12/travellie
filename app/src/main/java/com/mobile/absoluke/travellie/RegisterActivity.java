8ee95819cd1fc8d97faf2491b39ec80e90c54d4e
Quan Dao <daonguyenquan@hotmail.com>
2017-10-17 19:08:13 +0700

    init login activity

--- app/src/main/java/com/mobile/absoluke/travellie/RegisterActivity.java
+++ app/src/main/java/com/mobile/absoluke/travellie/RegisterActivity.java
@@ -3,11 +3,11 @@ package com.mobile.absoluke.travellie;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginXMLActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loginxml);
+        setContentView(R.layout.activity_register);
     }
 }

a689fe86f8db28b54d4c353593c4841e07efaaa1
Kiet Vo <geforce2409@gmail.com>
2017-11-10 09:40:54 +0700

    Thêm Fragment, viewPager cho activity Profile (Update thêm file)

---
+++ app/src/main/java/com/mobile/absoluke/travellie/FragmentLocation.java
@@ -0,0 +1,20 @@
+package com.mobile.absoluke.travellie;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Yul Lucia on 11/07/2017.
+ */
+
+public class FragmentLocation extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_location, container, false);
+    }
+}
---
+++ app/src/main/java/com/mobile/absoluke/travellie/PagerAdapter.java
@@ -0,0 +1,67 @@
+package com.mobile.absoluke.travellie;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by Yul Lucia on 11/09/2017.
+ */
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    public PagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                FragmentAbout fragmentAbout = new FragmentAbout();
+
+                return fragmentAbout;
+
+            case 1:
+                FragmentLocation fragmentLocation = new FragmentLocation();
+
+                return fragmentLocation;
+
+            case 2:
+                FragmentPhotos fragmentPhotos = new FragmentPhotos();
+
+                return fragmentPhotos;
+
+            case 3:
+                FragmentPosts fragmentPosts = new FragmentPosts();
+
+                return fragmentPosts;
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = "";
+        switch (position) {
+            case 0:
+                title = "About";
+                break;
+            case 1:
+                title = "Location";
+                break;
+            case 2:
+                title = "Photos";
+                break;
+            case 3:
+                title = "Posts";
+                break;
+        }
+        return title;
+    }
+}

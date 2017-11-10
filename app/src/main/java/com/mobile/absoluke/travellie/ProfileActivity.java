d62b863cb4491d3e8c5253f1cdb15ee7675070da
Kiet Vo <geforce2409@gmail.com>
2017-11-10 09:39:02 +0700

    Thêm Fragment, viewPager cho activity Profile

--- app/src/main/java/com/mobile/absoluke/travellie/ProfileActivity.java
+++ app/src/main/java/com/mobile/absoluke/travellie/ProfileActivity.java
@@ -1,16 +1,35 @@
 package com.mobile.absoluke.travellie;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    TabLayout tabLayout;
+    private PagerAdapter pagerAdapter;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        pagerAdapter = new PagerAdapter(getSupportFragmentManager());
+
+        viewPager = findViewById(R.id.container);
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setTabsFromPagerAdapter(pagerAdapter);
+
     }
 }

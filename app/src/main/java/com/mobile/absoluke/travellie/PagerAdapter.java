a93982555d5dea95317e89a19900bf64633264be
Kiet Vo <geforce2409@gmail.com>
2017-11-16 15:56:30 +0700

    Update cho PageAdapter Sửa CollapsingToolbarLayout cho ImageView và Avatar.

--- app/src/main/java/com/mobile/absoluke/travellie/PagerAdapter.java
+++ app/src/main/java/com/mobile/absoluke/travellie/PagerAdapter.java
@@ -3,6 +3,10 @@ package com.mobile.absoluke.travellie;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Yul Lucia on 11/09/2017.
@@ -10,6 +14,7 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 
 public class PagerAdapter extends FragmentStatePagerAdapter {
 
+    String[] listFragment = {"FragmentLocation", "FragmentPosts", "FragmentPhotos", "FragmentAbout"};
     public PagerAdapter(FragmentManager fm) {
         super(fm);
     }
@@ -18,14 +23,14 @@ public class PagerAdapter extends FragmentStatePagerAdapter {
     public Fragment getItem(int position) {
         switch (position) {
             case 0:
-                FragmentAbout fragmentAbout = new FragmentAbout();
-
-                return fragmentAbout;
-
-            case 1:
                 FragmentLocation fragmentLocation = new FragmentLocation();
 
                 return fragmentLocation;
+
+            case 1:
+                FragmentPosts fragmentPosts = new FragmentPosts();
+
+                return fragmentPosts;
 
             case 2:
                 FragmentPhotos fragmentPhotos = new FragmentPhotos();
@@ -33,33 +38,33 @@ public class PagerAdapter extends FragmentStatePagerAdapter {
                 return fragmentPhotos;
 
             case 3:
-                FragmentPosts fragmentPosts = new FragmentPosts();
+                FragmentAbout fragmentAbout = new FragmentAbout();
 
-                return fragmentPosts;
+                return fragmentAbout;
         }
         return null;
     }
 
     @Override
     public int getCount() {
-        return 4;
+        return listFragment.length;
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
-//        String title = "";
+        String title = "";
 //        switch (position) {
 //            case 0:
-//                title = "About";
+//                title = "Location";
 //                break;
 //            case 1:
-//                title = "Location";
+//                title = "Posts";
 //                break;
 //            case 2:
 //                title = "Photos";
 //                break;
 //            case 3:
-//                title = "Posts";
+//                title = "About";
 //                break;
 //        }
 //        return title;

33971eba94216b1af72949c015a6c8f10661cb43
Quan Dao <daonguyenquan@hotmail.com>
2017-12-20 14:41:54 +0700

    working: main_bottom_bar

---
+++ app/src/main/java/com/mobile/absoluke/travellie/ScrollingToolbarBehavior.java
@@ -0,0 +1,37 @@
+package com.mobile.absoluke.travellie;
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by SAGER on 20-Dec-17.
+ */
+class ScrollingToolbarBehavior extends CoordinatorLayout.Behavior<Toolbar> {
+
+    public ScrollingToolbarBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, Toolbar child, View dependency) {
+        return dependency instanceof AppBarLayout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, Toolbar child, View dependency) {
+        if (dependency instanceof AppBarLayout) {
+
+            int distanceToScroll = child.getHeight();
+
+            int bottomToolbarHeight = child.getHeight();//TODO replace this with bottom toolbar height.
+
+            float ratio = dependency.getY() / (float) bottomToolbarHeight;
+            child.setTranslationY(-distanceToScroll * ratio);
+        }
+        return true;
+    }
+}
